c0e9839e06e5f8532aecb8e006d8cbf607bf1e30
Susovan <susovan.sahoo777@gmail.com>
2024-08-26 21:47:04 +0530

    Modified the app with logic

--- 03-date-counter/src/Counter.jsx
+++ 03-date-counter/src/Counter.jsx
@@ -1,7 +1,45 @@
 import React, { useState } from "react";
 
 const Counter = () => {
-  
+  const today = new Date().toDateString();
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const [date, setDate] = useState(today);
+
+  const increaseStep = () => {
+    setStep((prev) => prev + 1);
+  };
+  const decreaseStep = () => {
+    if (step >= 2) setStep((prev) => prev - 1);
+  };
+  const increaseCount = () => {
+    setCount((prev) => prev + step);
+  };
+  const decreaseCount = () => {
+    setCount((prev) => prev - step);
+  };
+  //   console.log(today);
+  return (
+    <>
+      <div>
+        <button onClick={decreaseStep}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={increaseStep}>+</button>
+      </div>
+      <div>
+        <button onClick={decreaseCount}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={increaseCount}>+</button>
+      </div>
+      {count ? (
+        <p>
+          {count} days from today is {date}
+        </p>
+      ) : (
+        <p>Today is {date}</p>
+      )}
+    </>
+  );
 };
 
 export default Counter;
