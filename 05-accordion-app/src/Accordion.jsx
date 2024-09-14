8a2b61f430cd3c2c94f2eab90a792b28de11436f
Susovan <susovan.sahoo777@gmail.com>
2024-09-14 23:45:03 +0530

    Accordion works fine

--- 05-accordion-app/src/Accordion.jsx
+++ 05-accordion-app/src/Accordion.jsx
@@ -1,23 +1,37 @@
+import { useState } from "react";
 import faqs from "./Faq-Data";
 
 const Accordion = () => {
   return (
     <div className="accordion">
-      <AccordionItem point="01" title="Where are these chairs assembled?" />
-      <AccordionItem
-        point="02"
-        title="How long do I have to return my chair?"
-      />
+      {faqs.map((el, i) => {
+        return (
+          <AccordionItem
+            key={i + 1}
+            point={i + 1}
+            title={el.title}
+            description={el.text}
+            // isExpand={isExpand}
+            // clickEvent={expandItem}
+          />
+        );
+      })}
     </div>
   );
 };
 
 const AccordionItem = ({ point, title, description }) => {
+  const [isExpand, setIsExpand] = useState(false);
+
+  const expandItem = () => {
+    setIsExpand((isExpand) => !isExpand);
+  };
   return (
-    <div className="item">
-      <h1>{point}</h1>
+    <div className={isExpand ? "item active" : "item"} onClick={expandItem}>
+      <h1 className="number">{point < 10 ? `0${point}` : { point }}</h1>
       <h1>{title}</h1>
-      <span>+</span>
+      <span>{!isExpand ? "+" : "-"}</span>
+      {isExpand && <p className="content">{description}</p>}
     </div>
   );
 };
