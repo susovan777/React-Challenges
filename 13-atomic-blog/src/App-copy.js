6fabd5a8309535d08f137e38e515066e015f680f
Susovan <susovan.sahoo777@gmail.com>
2024-11-28 16:27:02 +0530

    Created separate PostContext.jsx file and usePost() custom hook

--- 13-atomic-blog/src/App-copy.js
+++ 13-atomic-blog/src/App-copy.js
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import { PostProvider, usePosts } from "./PostContext";
+import { createContext, useContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -9,8 +8,33 @@ function createRandomPost() {
   };
 }
 
+// 1️⃣ Create a new Context
+const PostContext = createContext();
+
 function App() {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
 
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
@@ -21,27 +45,37 @@ function App() {
   );
 
   return (
-    <section>
-      <button
-        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-        className="btn-fake-dark-mode"
-      >
-        {isFakeDark ? "☀️" : "🌙"}
-      </button>
+    // 2️⃣ Provide Value to Child
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      <section>
+        <button
+          onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+          className="btn-fake-dark-mode"
+        >
+          {isFakeDark ? "☀️" : "🌙"}
+        </button>
 
-      <PostProvider>
         <Header />
         <Main />
         <Archive />
         <Footer />
-      </PostProvider>
-    </section>
+      </section>
+    </PostContext.Provider>
   );
 }
 
 function Header() {
   // 3️⃣ Consuming Context Value
-  const { posts, searchQuery, setSearchQuery, onClearPosts } = usePosts();
+  const { posts, searchQuery, setSearchQuery, onClearPosts } =
+    useContext(PostContext);
 
   return (
     <header>
@@ -62,7 +96,7 @@ function Header() {
 
 function SearchPosts() {
   // 3️⃣ Consuming Context Value
-  const { searchQuery, setSearchQuery } = usePosts();
+  const { searchQuery, setSearchQuery } = useContext(PostContext);
 
   return (
     <input
@@ -75,7 +109,7 @@ function SearchPosts() {
 
 function Results() {
   // 3️⃣ Consuming Context Value
-  const { posts } = usePosts();
+  const { posts } = useContext(PostContext);
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
@@ -127,7 +161,7 @@ function FormAddPost({ onAddPost }) {
 
 function List() {
   // 3️⃣ Consuming Context Value
-  const { posts } = usePosts();
+  const { posts } = useContext(PostContext);
   return (
     <ul>
       {posts.map((post, i) => (
@@ -143,7 +177,7 @@ function List() {
 function Archive() {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   // 3️⃣ Consuming Context Value
-  const { onAddPost } = usePosts();
+  const { onAddPost } = useContext(PostContext);
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 100 }, () => createRandomPost())
